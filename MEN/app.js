2ecf0e0934f890db987e4f997b7c3090ed79266b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-15 01:39:09 +0530

    refactor server response handling and improve code formatting

--- MEN/app.js
+++ MEN/app.js
@@ -1,10 +1,17 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end('Hello World');
-})
+    if (req.url == "/") {
+        res.end("Home Page");
+    }
+    if (req.url == "/about") {
+        res.end("About Page");
+    }
+    if (req.url == "/profile") {
+        res.end("Profile Page");
+    }
+});
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log("Server is running on port 3000");
 });
