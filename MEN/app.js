8255e599a0d81fdf2fbcdff3a4a989df10768fd4
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-14 15:15:09 +0530

    replace cat-me with a basic HTTP server implementation

--- MEN/app.js
+++ MEN/app.js
@@ -1,3 +1,9 @@
-const catMe = require('cat-me')
+const http = require('http');
 
-console.log(catMe())
+const server = http.createServer((req, res) => {
+    res.end('Hello World');
+})
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
