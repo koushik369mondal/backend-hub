da952242c2001b97fedf45b6bab19242675e46ad
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-17 22:59:31 +0530

    implement input validation for user registration

--- DRIVE/routes/user.routes.js
+++ DRIVE/routes/user.routes.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 router.get("/register", (req, res) => {
     res.render("register");
 })
 
-router.post("/register", (req, res) => {
-    console.log(req.body);
-    res.send("User registered successfully");
-})
+router.post("/register",
+    body('email').trim().isEmail().isLength({ min: 10 }),
+    body('password').trim().isLength({ min: 5 }),
+    body("username").trim().isLength({ min: 3 }),
+    (req, res) => {
+
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.send("Invalid input");
+        }
+
+        res.send(errors)
+    })
 
 
 
