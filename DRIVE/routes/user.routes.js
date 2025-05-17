1d706281f9d42a3fc7ecb4a14b7127f13adf36e1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-17 20:19:08 +0530

    add register route and create register.ejs view

--- DRIVE/routes/user.routes.js
+++ DRIVE/routes/user.routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    res.send("User Test route");
+router.get("/register", (req, res) => {
+    res.render("register");
 })
 
 
