de42694e35f81b5c206b766e783e4885f9f389f0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-18 00:57:31 +0530

    add cookie-parser middleware and set JWT token in cookies on login

--- DRIVE/app.js
+++ DRIVE/app.js
@@ -4,10 +4,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const connectToDB = require("./config/db");
 connectToDB();
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
