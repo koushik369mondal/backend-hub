8be05423e4c31a88ae2da450d2b8dd02f0e4a912
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-17 23:31:27 +0530

    add dotenv configuration and MongoDB connection setup

--- DRIVE/app.js
+++ DRIVE/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const userRouter = require("./routes/user.routes");
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDB = require("./config/db");
+connectToDB();
 
 const app = express();
 
