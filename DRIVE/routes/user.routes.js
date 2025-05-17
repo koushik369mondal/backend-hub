8b869d57614af849a7f01782546e65d9f0dd5400
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-18 00:24:47 +0530

    add login route and view with validation and password comparison

--- DRIVE/routes/user.routes.js
+++ DRIVE/routes/user.routes.js
@@ -37,8 +37,44 @@ router.post("/register",
 
     })
 
+router.get("/login", (req, res) => {
+    res.render("login");
+})
 
+router.post("/login",
+    body('username').trim().isLength({ min: 3 }),
+    body('password').trim().isLength({ min: 5 }),
+    async (req, res) => {
 
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Invalid data"
+            })
+        }
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne({
+            username: username
+        });
+        if (!user) {
+            return res.status(400).json({
+                message: "username or password is incorrect"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "username or password is incorrect"
+            })
+        }
+
+        
+    }
+)
 
 
 module.exports = router;
