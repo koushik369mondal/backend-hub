8bec507fae1a8c929d48ef051ee68a8196aa5402
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-18 09:55:12 +0530

    add index route and home view

--- DRIVE/app.js
+++ DRIVE/app.js
@@ -6,12 +6,14 @@ const connectToDB = require("./config/db");
 connectToDB();
 const cookieParser = require('cookie-parser');
 const app = express();
+const indexRouter = require("./routes/index.routes");
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+app.use("/", indexRouter);
 app.use("/user", userRouter);
 
 app.listen(3000, () => {
